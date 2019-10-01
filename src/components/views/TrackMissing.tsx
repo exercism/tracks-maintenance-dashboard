8602096c54f2fca61a4a1a44731ca44b462125ad
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-10-01 22:18:45 +0200

    Add missing icons

--- src/components/views/TrackMissing.tsx
+++ src/components/views/TrackMissing.tsx
@@ -2,6 +2,7 @@ import React, { useMemo, useCallback } from 'react'
 import { useRemoteConfig } from '../../hooks/useRemoteConfig'
 import { useRemoteCanonicalList } from '../../hooks/useRemoteCanonicalList'
 import { LoadingIndicator } from '../LoadingIndicator'
+import { transform } from '@babel/core'
 
 const NO_EXERCISES = Object.freeze([])
 
@@ -60,8 +61,37 @@ function ExerciseList({
     (exercise: ExerciseConfiguration['slug']) => {
       const canonicalUrl = `https://github.com/exercism/problem-specifications/tree/master/exercises/${exercise}`
       return (
-        <li key={exercise}>
-          <a href={canonicalUrl}>{exercise}</a>
+        <li key={exercise} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-2">
+          <a href={canonicalUrl} className="d-flex align-items-center">
+            <figure className="media mr-2 mb-0">
+              <img className="hover"
+                src={`https://assets.exercism.io/exercises/${exercise}-white.png`}
+                alt={`${exercise} logo`}
+                style={{
+                  background: '#009cab',
+                  border: '1px solid rgba(0,156,171,0.5)',
+                  padding: 6,
+                  width: 56,
+                  borderRadius: 2,
+                  height: 56
+                }}
+                />
+
+              <img className="normal"
+                src={`https://assets.exercism.io/exercises/${exercise}-turquoise.png`}
+                alt={`${exercise} logo hover state`}
+                style={{
+                  background: '#fff',
+                  border: '1px solid rgba(0,156,171,0.5)',
+                  padding: 6,
+                  borderRadius: 2,
+                  width: 56,
+                  height: 56
+                }}
+                />
+            </figure>
+            <span className="media-body">{exercise}</span>
+          </a>
         </li>
       )
     },
@@ -72,5 +102,39 @@ function ExerciseList({
     return <div>No exercises found</div>
   }
 
-  return <ol className="list">{exercises.map(renderExercise)}</ol>
+  return (
+    <>
+      <style>{`
+        .exercises-list > li > a {
+          color: #333;
+        }
+
+        .exercises-list > li > a figure {
+          position: relative;
+          width: 56px;
+          height: 56px;
+          overflow: hidden;
+        }
+
+        .exercises-list > li > a img {
+          display: inline-block;
+        }
+
+        .exercises-list > li > a img.hover {
+          will-transform: opacity;
+          opacity: 0;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+        }
+
+        .exercises-list > li > a:hover img.hover {
+          opacity: 1;
+        }
+      `}</style>
+      <ol className="exercises-list list-unstyled row mt-2 mb-2">{exercises.map(renderExercise)}</ol>
+    </>
+  )
 }
