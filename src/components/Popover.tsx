88b76722eed9ccc31c6539f5677b952abe58fdb2
Tucker <trblackw@ncsu.edu>
2019-10-05 16:14:39 -0400

    Set up track aside and track checklist components to toggle visibility on outside click

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -1,73 +1,70 @@
-import React, { useState, useCallback } from 'react'
-import { LoadingIndicator } from './LoadingIndicator'
-import { CheckOrCross } from './CheckOrCross'
+import React, { forwardRef, Ref } from 'react';
+import { LoadingIndicator } from './LoadingIndicator';
+import { CheckOrCross } from './CheckOrCross';
 
 interface PopoverProps {
-  align?: 'center' | 'right'
-  title?: React.ReactNode;
-  active?: boolean;
-  children: React.ReactNode;
+   align?: 'center' | 'right';
+   title?: React.ReactNode;
+   active?: boolean;
+   children: React.ReactNode;
 }
 
 export function Popover({ children, active, title, align }: PopoverProps) {
-  const left = align === 'right' ? -253 : -150
-  const arrow = align === 'right' ? 255 : 153
+   const left = align === 'right' ? -253 : -150;
+   const arrow = align === 'right' ? 255 : 153;
 
-  return (
-    <div className={`popover fade ${active !== false && 'show'} bs-popover-bottom`} role="tooltip" style={{
-      position: 'absolute',
-      top: 25,
-      left,
-      width: 300,
-      maxWidth: 300,
-      pointerEvents: active ? 'auto' : 'none'
-    }}>
-      <div className="arrow" style={{ left: arrow }}></div>
-      {title && <h5 className="popover-title">{title}</h5>}
-      <div className="popover-body">
-        {children}
+   return (
+      <div
+         className={`popover fade ${active !== false && 'show'} bs-popover-bottom`}
+         role='tooltip'
+         style={{
+            position: 'absolute',
+            top: 25,
+            left,
+            width: 300,
+            maxWidth: 300,
+            pointerEvents: active ? 'auto' : 'none'
+         }}>
+         <div className='arrow' style={{ left: arrow }} />
+         {title && <h5 className='popover-title'>{title}</h5>}
+         <div className='popover-body'>{children}</div>
       </div>
-    </div>
-  )
+   );
 }
 
-export function ContainedPopover({ children, title, toggle, active, align, onToggle }: PopoverProps & { onToggle?: () => void; toggle: React.ReactNode }) {
-  const [isActive, setActive] = useState(active === true)
-  const doToggleActive = useCallback(() => onToggle ? onToggle() : setActive((prev) => !prev), [onToggle])
-
-  return (
-    <div style={{ position: 'relative', display: 'inline-block'}}>
-      <button type="button" style={{ background: 0, border: 0 }} onClick={doToggleActive}>
-        {toggle}
+export const ContainedPopover = ({
+   children,
+   title,
+   toggle,
+   active,
+   align,
+   onToggle
+}: PopoverProps & { onToggle?: () => void; toggle: React.ReactNode }) => (
+   <div style={{ position: 'relative', display: 'inline-block' }}>
+      <button type='button' style={{ background: 0, border: 0 }} onClick={onToggle}>
+         {toggle}
       </button>
-      <Popover active={active !== undefined ? active : isActive} title={title} align={align}>{children}</Popover>
-    </div>
-  )
-}
+      <Popover active={active} title={title} align={align}>
+         {children}
+      </Popover>
+   </div>
+);
 
 interface IconWithPopoverProps {
-  loading: boolean;
-  valid: boolean;
-  children: React.ReactNode;
-  active?: boolean;
-  onToggle?(): void;
+   loading: boolean;
+   valid: boolean;
+   children: React.ReactNode;
+   active?: boolean;
+   onToggle?(): void;
 }
 
-export function LoadingIconWithPopover({ active, onToggle, loading, valid, children }: IconWithPopoverProps) {
-  if (loading) {
-    return (
-      <button type="button" style={{ background: 0, border: 0}}>
-        <LoadingIndicator />
+export const LoadingIconWithPopover = ({ active, onToggle, loading, valid, children }: IconWithPopoverProps) =>
+   loading ? (
+      <button type='button' style={{ background: 0, border: 0 }}>
+         <LoadingIndicator />
       </button>
-    )
-  }
-
-  return (
-    <ContainedPopover active={active}
-      onToggle={onToggle}
-      align="right"
-      toggle={<CheckOrCross value={valid} />}>
-      {children}
-    </ContainedPopover>
-  )
-}
+   ) : (
+      <ContainedPopover active={active} onToggle={onToggle} align='right' toggle={<CheckOrCross value={valid} />}>
+         {children}
+      </ContainedPopover>
+   );
--- src/components/TrackChecklist.tsx
+++ src/components/TrackChecklist.tsx
@@ -1,123 +1,118 @@
-import React, { useCallback } from 'react'
-import { useToggleState } from '../hooks/useToggleState'
-import { useTrackAsideData } from '../hooks/useTrackData'
-import { LoadingIconWithPopover } from './Popover'
+import React, { useCallback, useState } from 'react';
+import { useToggleState } from '../hooks/useToggleState';
+import { useTrackAsideData } from '../hooks/useTrackData';
+import { LoadingIconWithPopover } from './Popover';
+import { useOutsideClick } from '../hooks/useOutsideClick';
 
-export function TrackChecklist({ trackId }: { trackId: TrackIdentifier }) {
-  const { done, checklist } = useTrackAsideData(trackId)
-
-  const [currentDetails, doToggleDetails] = useToggleState()
-
-  return (
-    <aside className="mt-md-4 mb-4 col-md">
-      <ul className="list-group" style={{ whiteSpace: 'nowrap' }}>
-        <li className="list-group-item d-flex justify-content-between">
-          Track blurb
-          <BlurbIcon currentDetails={currentDetails} onToggleDetails={doToggleDetails} loading={!done} valid={checklist.hasBlurb} />
-        </li>
-        <li className="list-group-item d-flex justify-content-between">
-          Auto approve exercise
-          <AutoApproveIcon currentDetails={currentDetails} onToggleDetails={doToggleDetails} loading={!done} valid={checklist.hasAutoApprove} />
-        </li>
-        <li className="list-group-item d-flex justify-content-between">
-          Exercises in core
-          <CoreIcon currentDetails={currentDetails} onToggleDetails={doToggleDetails} loading={!done} valid={checklist.exerciseCoreCount > 0} />
-        </li>
-        <li className="list-group-item d-flex justify-content-between">
-          Exercises with topics
-          <TopicsIcon currentDetails={currentDetails} onToggleDetails={doToggleDetails} loading={!done} valid={checklist.exerciseWithTopicsCount > 0} />
-        </li>
-      </ul>
-    </aside>
-  )
-}
-
+export const TrackChecklist = ({ trackId }: { trackId: TrackIdentifier }) => {
+   const { done, checklist } = useTrackAsideData(trackId);
+   //track-blurb
+   const [trackBlurbVisible, setTrackBlurbVisible] = useState<boolean>(false);
+   const trackBlurbRef = useOutsideClick(() => setTrackBlurbVisible(false));
+   //auto-approve-exercise
+   const [approveExerciseVisible, setApproveExerciseVisible] = useState<boolean>(false);
+   const approveExerciseVisibleRef = useOutsideClick(() => setApproveExerciseVisible(false));
+   //exercises-in-core
+   const [exercisesInCoreVisible, setExercisesInCoreVisible] = useState<boolean>(false);
+   const exerciseInCoreRef = useOutsideClick(() => setExercisesInCoreVisible(false));
+   //exercises-with-topics
+   const [exercisesWithTopicsVisible, setExercisesWithTopicsVisible] = useState<boolean>(false);
+   const exerciseWithTopicsRef = useOutsideClick(() => setExercisesWithTopicsVisible(false));
+   return (
+      <aside className='mt-md-4 mb-4 col-md'>
+         <ul className='list-group' style={{ whiteSpace: 'nowrap' }}>
+            <li className='list-group-item d-flex justify-content-between' ref={trackBlurbRef}>
+               Track blurb
+               <BlurbIcon
+                  onToggleDetails={() => setTrackBlurbVisible(prev => !prev)}
+                  loading={!done}
+                  valid={checklist.hasBlurb}
+                  contentVisible={trackBlurbVisible}
+               />
+            </li>
+            <li className='list-group-item d-flex justify-content-between' ref={approveExerciseVisibleRef}>
+               Auto approve exercise
+               <AutoApproveIcon
+                  onToggleDetails={() => setApproveExerciseVisible(prev => !prev)}
+                  loading={!done}
+                  valid={checklist.hasAutoApprove}
+                  contentVisible={approveExerciseVisible}
+               />
+            </li>
+            <li className='list-group-item d-flex justify-content-between' ref={exerciseInCoreRef}>
+               Exercises in core
+               <CoreIcon
+                  onToggleDetails={() => setExercisesInCoreVisible(prev => !prev)}
+                  loading={!done}
+                  valid={checklist.exerciseCoreCount > 0}
+                  contentVisible={exercisesInCoreVisible}
+               />
+            </li>
+            <li className='list-group-item d-flex justify-content-between' ref={exerciseWithTopicsRef}>
+               Exercises with topics
+               <TopicsIcon
+                  onToggleDetails={() => setExercisesWithTopicsVisible(prev => !prev)}
+                  loading={!done}
+                  valid={checklist.exerciseWithTopicsCount > 0}
+                  contentVisible={exercisesWithTopicsVisible}
+               />
+            </li>
+         </ul>
+      </aside>
+   );
+};
 
 interface PreconfiguredIconProps {
-  loading: boolean;
-  valid: boolean;
-  currentDetails?: string;
-  onToggleDetails?(next: string): void;
+   loading: boolean;
+   valid: boolean;
+   onToggleDetails: () => void;
+   contentVisible: boolean;
 }
 
-function BlurbIcon({ loading, valid, currentDetails, onToggleDetails }: PreconfiguredIconProps) {
-  const active = currentDetails === 'blurb'
-  const doToggle = useCallback(() => onToggleDetails && onToggleDetails('blurb'), [onToggleDetails])
-
-  return (
-    <LoadingIconWithPopover active={active}
-      loading={loading}
-      valid={valid}
-      onToggle={doToggle}>
+const BlurbIcon = ({ loading, valid, onToggleDetails, contentVisible }: PreconfiguredIconProps) => (
+   <LoadingIconWithPopover active={contentVisible} loading={loading} valid={valid} onToggle={onToggleDetails}>
       <p>
-        This check passes if there is a <code>blurb</code> present in the <code>config.json</code> file,
-        which in turn is located at the root of the repository. The <code>blurb</code> is a <strong>requirement</strong>.
+         This check passes if there is a <code>blurb</code> present in the <code>config.json</code> file, which in turn is located at the
+         root of the repository. The <code>blurb</code> is a <strong>requirement</strong>.
       </p>
       <AboutConfigJson />
-    </LoadingIconWithPopover>
-  )
-}
+   </LoadingIconWithPopover>
+);
 
-function AutoApproveIcon({ loading, valid, currentDetails, onToggleDetails }: PreconfiguredIconProps) {
-  const active = currentDetails === 'auto-approve'
-  const doToggle = useCallback(() => onToggleDetails && onToggleDetails('auto-approve'), [onToggleDetails])
-
-  return (
-    <LoadingIconWithPopover active={active}
-      loading={loading}
-      valid={valid}
-      onToggle={doToggle}>
+const AutoApproveIcon = ({ loading, valid, onToggleDetails, contentVisible }: PreconfiguredIconProps) => (
+   <LoadingIconWithPopover active={contentVisible} loading={loading} valid={valid} onToggle={onToggleDetails}>
       <p>
-        This check passes if there is at least one exercise with the <code>auto_approve</code> flag switched to <code>true</code>.
-        In general, this is the <em>first core</em> exercise of the track, usually <code>hello-world</code>.
+         This check passes if there is at least one exercise with the <code>auto_approve</code> flag switched to <code>true</code>. In
+         general, this is the <em>first core</em> exercise of the track, usually <code>hello-world</code>.
       </p>
       <AboutConfigJson />
-    </LoadingIconWithPopover>
-  )
-}
+   </LoadingIconWithPopover>
+);
 
-function CoreIcon({ loading, valid, currentDetails, onToggleDetails }: PreconfiguredIconProps) {
-  const active = currentDetails === 'core'
-  const doToggle = useCallback(() => onToggleDetails && onToggleDetails('core'), [onToggleDetails])
-
-  return (
-  <LoadingIconWithPopover active={active}
-      loading={loading}
-      valid={valid}
-      onToggle={doToggle}>
+const CoreIcon = ({ loading, valid, contentVisible, onToggleDetails }: PreconfiguredIconProps) => (
+   <LoadingIconWithPopover active={contentVisible} loading={loading} valid={valid} onToggle={onToggleDetails}>
       <p>
-        This check passes if there is at least one <code>core</code> exercise in the <code>config.json</code>.
-        Having a <em>core</em> track is <strong>required</strong>. Having non-core exercises (side exercises) is
-        optional.
+         This check passes if there is at least one <code>core</code> exercise in the <code>config.json</code>. Having a <em>core</em> track
+         is <strong>required</strong>. Having non-core exercises (side exercises) is optional.
       </p>
 
       <AboutConfigJson />
-    </LoadingIconWithPopover>
-  )
-}
+   </LoadingIconWithPopover>
+);
 
-function TopicsIcon({ loading, valid, currentDetails, onToggleDetails }: PreconfiguredIconProps) {
-  const active = currentDetails === 'topics'
-  const doToggle = useCallback(() => onToggleDetails && onToggleDetails('topics'), [onToggleDetails])
-
-  return (
-    <LoadingIconWithPopover active={active}
-      loading={loading}
-      valid={valid}
-      onToggle={doToggle}>
+const TopicsIcon = ({ loading, valid, contentVisible, onToggleDetails }: PreconfiguredIconProps) => (
+   <LoadingIconWithPopover active={contentVisible} loading={loading} valid={valid} onToggle={onToggleDetails}>
       <p>
-        This check passes if there is at least one exercise in the <code>config.json</code> with 1 or more topics.
+         This check passes if there is at least one exercise in the <code>config.json</code> with 1 or more topics.
       </p>
 
       <AboutConfigJson />
-    </LoadingIconWithPopover>
-  )
-}
+   </LoadingIconWithPopover>
+);
 
-function AboutConfigJson() {
-  return (
-    <p className="mb-0">
-      You can find more information about the <code>config.json</code> file <a href="https://github.com/exercism/problem-specifications/blob/master/CONTRIBUTING.md#track-configuration-file">here</a>.
-    </p>
-  )
-}
+const AboutConfigJson = () => (
+   <p className='mb-0'>
+      You can find more information about the <code>config.json</code> file{' '}
+      <a href='https://github.com/exercism/problem-specifications/blob/master/CONTRIBUTING.md#track-configuration-file'>here</a>.
+   </p>
+);
