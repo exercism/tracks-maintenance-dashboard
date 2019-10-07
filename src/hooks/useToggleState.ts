3f287a7d9b6b31080911f89f94753ce9728a081a
Tucker <trblackw@ncsu.edu>
2019-10-07 18:50:03 -0400

    Combine newly created useOutsideClick hook with useToggleState hook

--- src/hooks/useToggleState.ts
+++ src/hooks/useToggleState.ts
@@ -1,4 +1,41 @@
-import { useState, useCallback } from 'react'
+import {
+  useState,
+  useCallback,
+  MutableRefObject,
+  useRef,
+  useEffect,
+  MouseEvent,
+} from 'react'
+/**
+ * Listens for outside clicks tied to a specific element
+ *
+ * - used in conjunction with useToggleState
+ *
+ * @param onClick
+ */
+function useOutsideClick<T extends HTMLElement>(
+  onClick: () => void
+): MutableRefObject<T> {
+  const ref = useRef<T>()
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent<T>): void => {
+      if (ref && ref.current && ref.current.contains(e.target as Node)) {
+        return
+      }
+      onClick()
+    }
+    //@ts-ignore
+    document.addEventListener('mousedown', handleOutsideClick)
+
+    return () => {
+      //@ts-ignore
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [onClick])
+
+  return ref as MutableRefObject<T>
+}
 
 /**
  * Returns a state and a toggler that allows setting the state
@@ -8,11 +45,19 @@ import { useState, useCallback } from 'react'
  *
  * @param initial
  */
-export function useToggleState(initial?: string): [string | undefined, (next: string) => void] {
+export function useToggleState<T extends HTMLElement = HTMLElement>(
+  initial?: string
+): [string | undefined, (next: string) => void, MutableRefObject<T>] {
   const [current, setCurrent] = useState(initial)
-  const doToggle = useCallback((next: string) => {
-    setCurrent((prev) => prev === next ? undefined : next)
-  }, [setCurrent])
+  const doToggle = useCallback(
+    (next: string) => {
+      setCurrent((prev) => (prev === next ? undefined : next))
+    },
+    [setCurrent]
+  )
 
-  return [current, doToggle]
+  const doUntoggle = useCallback(() => setCurrent(undefined), [setCurrent])
+  const ref = useOutsideClick<T>(doUntoggle)
+
+  return [current, doToggle, ref]
 }
